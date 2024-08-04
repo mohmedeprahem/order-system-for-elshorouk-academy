84c60fad1f103e477eb72c90f019b8ee02ee0a27
mohmedeprahem <m.eprahem.168@gmail.com>
2024-08-04 23:48:21 +0300

    Build register page

--- src/src/Controllers/AuthController.cs
+++ src/src/Controllers/AuthController.cs
@@ -5,9 +5,16 @@ namespace src.Controllers
 {
     public class AuthController : Controller
     {
+        [Route("/")]
+        [Route("[controller]/Login")]
         public ActionResult Index()
         {
             return View("Login");
+        }
+
+        public ActionResult Register()
+        {
+            return View("Register");
         }
     }
 }
