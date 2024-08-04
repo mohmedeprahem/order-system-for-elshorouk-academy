a997a3041e3d3c67d38a28714b2ae79b6c1d8ada
mohmedeprahem <m.eprahem.168@gmail.com>
2024-08-04 23:37:04 +0300

    Build Login page

---
+++ src/src/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace src.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+    }
+}
