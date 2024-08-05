f37b85da2bc474fbeced344a257f926dad5665e4
mohmedeprahem <m.eprahem.168@gmail.com>
2024-08-05 23:07:10 +0300

    Feat: Create create invoiceDetail post api

---
+++ src/src/Controllers/InvoiceDetailController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using src.Models;
+using src.Repositories;
+using src.ViewModels;
+
+namespace src.Controllers
+{
+    public class InvoiceDetailController : Controller
+    {
+        private readonly InvoiceRepository _invoiceRepository;
+        private readonly UnitOfWork _unitOfWork;
+
+        public InvoiceDetailController(InvoiceRepository invoiceRepository, UnitOfWork unitOfWork)
+        {
+            _invoiceRepository = invoiceRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([FromBody] InvoiceDetailCreateModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var invoice = await _unitOfWork.InvoiceRepository.GetInvoiceById(model.InvoiceHeaderId);
+
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+
+            var invoiceDetail = new InvoiceDetail
+            {
+                ItemName = model.ItemName,
+                ItemPrice = model.ItemPrice,
+                ItemCount = model.ItemCount,
+                InvoiceHeaderId = model.InvoiceHeaderId
+            };
+
+            await _unitOfWork.InvoiceRepository.AddInvoiceDetail(invoiceDetail);
+
+            await _unitOfWork.SaveChangesAsync();
+
+            return Ok(new { ItemId = invoiceDetail.Id });
+        }
+    }
+}
---
+++ src/src/ViewModels/InvoiceDetailCreateModel.cs
@@ -0,0 +1,10 @@
+﻿namespace src.ViewModels
+{
+    public class InvoiceDetailCreateModel
+    {
+        public long InvoiceHeaderId { get; set; }
+        public string ItemName { get; set; }
+        public float ItemCount { get; set; }
+        public float ItemPrice { get; set; }
+    }
+}
